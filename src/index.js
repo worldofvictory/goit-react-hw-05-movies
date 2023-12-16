ce983abdc7f2afb36383b16ceb6a25e29371a69c
Viktoriya <arizona081392@gmail.com>
2023-12-17 00:08:25 +0200

    ok

--- src/index.js
+++ src/index.js
@@ -2,9 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from '@emotion/react'
+import theme from './constants/theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ThemeProvider theme={theme}> 
+      <BrowserRouter basename="/goit-react-hw-05-movies">
+        <App />
+      </BrowserRouter>   
+    </ThemeProvider> 
+  </React.StrictMode>      
 );
+
+
